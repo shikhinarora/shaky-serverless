26e508a874cc7b583a4a1bb8fb5039874a6e5b3f
Shikhin Arora <shikhin.arora@gmail.com>
2018-10-23 14:52:03 +0530

    introduction complete

--- handler.js
+++ handler.js
@@ -4,8 +4,7 @@ module.exports.hello = async (event, context) => {
   return {
     statusCode: 200,
     body: JSON.stringify({
-      message: 'Go Serverless v1.0! Your function executed successfully!',
-      input: event,
+      message: 'Hello shaky!'
     }),
   };
 
